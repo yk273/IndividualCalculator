1b682b33ad3e59967239361c380edf6dab6f73f2
Yana Kokit <yk273@njit.edu>
2019-11-01 03:16:36 -0400

    Fix: modified csv files

--- src/csvTests.py
+++ src/csvTests.py
@@ -1,9 +1,18 @@
 import unittest
+from csvReader.CsvReader import CsvReader, class_factory
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+    def setUp(self) -> None:
+        self.csv_reader = CsvReader('Tests/Data/employee_birthday.csv')
+
+    def test_return_data_as_objects(self):
+        people = self.csv_reader.return_data_as_objects('person')
+        self.assertIsInstance(people, list)
+        test_class = class_factory('person', self.csv_reader.data[0])
+        for person in people:
+            self.assertEqual(person.__name__, test_class.__name__)
 
 
 if __name__ == '__main__':

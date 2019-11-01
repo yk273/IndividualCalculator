ea600f6810a885119c328ba83da1d8db19bc7f94
Yana Kokit <yk273@njit.edu>
2019-11-01 02:57:11 -0400

    Feature: added the 7 calculations to Calculator file, with static methods and class methods

---
+++ src/Calculator.py
@@ -0,0 +1,59 @@
+def addition(num1, num2):
+    sol = num1 + num2
+    return sol
+
+
+def subtraction(num1, num2):
+    sol = num2 - num1
+    return sol
+
+
+def multiplication(num1, num2):
+    sol = num1 * num2
+    return sol
+
+
+def division(num1, num2):
+    sol = num1 / num2
+    return sol
+
+
+def square(num1):
+    sol = num1 ** 2
+    return sol
+
+
+def square_root(num1):
+    sol = num1 ** 0.5
+    return sol
+
+
+class Calculate:
+    result = 0
+
+    def __init__(self):
+        pass
+
+    def add(self, num1, num2):
+        self.result = num1 + num2
+        return self.result
+
+    def subtract(self, num1, num2):
+        self.result = num1 - num2
+        return self.result
+
+    def multiply(self, num1, num2):
+        self.result = num1 * num2
+        return self.result
+
+    def divide(self, num1, num2):
+        self.result = num1 / num2
+        return self.result
+
+    def square(self, num1):
+        self.result = num1 ** 2
+        return self.result
+
+    def square_root(self, num1):
+        self.result = num1 ** 0.5
+        return self.result

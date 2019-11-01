6c36f14ef1b768eff0f12f06bd401f639e2bf7a7
Yana Kokit <yk273@njit.edu>
2019-11-01 03:07:28 -0400

    Fix: modified csvReader file

--- src/csvReader.py
+++ src/csvReader.py
@@ -1,7 +1,7 @@
 import csv
 
 
-def classFactory(class_name, dictionary):
+def class_factory(class_name, dictionary):
     return type(class_name, (object,), dictionary)
 
 
@@ -20,5 +20,5 @@ class CsvReader:
     def return_data_as_objects(self, class_name):
         objects = []
         for row in self.data:
-            objects.append(classFactory(class_name, row))
+            objects.append(class_factory(class_name, row))
         return objects
